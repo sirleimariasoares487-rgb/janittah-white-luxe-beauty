978f031aff9c15ff52c709305ab24e6b43b0d31d
sirleimariasoares487-rgb <sirleimariasoares487@gmail.com>
2026-09-07 17:19:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
   Use the project's publishable/anon key only.
   NEVER put a Supabase secret/service-role key in this file.
 */
-window.SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
-window.SUPABASE_PUBLISHABLE_KEY = "YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY";
+window.SUPABASE_URL = "https://wjxypmubddxmwxlnkytc.supabase.co";
+window.SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqeHlwbXViZGR4bXd4bG5reXRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3ODczMDEsImV4cCI6MjEwNDM2MzMwMX0.2-NJMm4bAApxLjWgdNrmD-HAzd-OkyVtTHWVV0YcT94";
